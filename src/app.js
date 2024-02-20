b4ec676aee94f7795f423cb538d4703fea7fed0c
andres fernandes <andresfernandesg10@gmail.com>
2024-02-20 02:22:25 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".web", ".edu"];
+
+  let dominio = function(pronoun, adj, noun, dom) {
+    let web = [];
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            web.push(pronoun[i] + adj[j] + noun[k] + dom[1]);
+          }
+        }
+      }
+    }
+    console.log(web);
+  };
+  dominio(pronoun, adj, noun, dom);
 };
